fef89f14af0e75a3ac788eb68e5287131d4ac5a7
Pavel Kishov <kishov.p@gmail.com>
2022-06-16 00:13:29 +0300

    completed HomeWork_01_task_04

--- HomeWork_01_task_04/Program.cs
+++ HomeWork_01_task_04/Program.cs
@@ -1,3 +1,18 @@
 ﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int N = Convert.ToInt32(ReadLine());
+int i = 1;
+
+while (i<N-2)
+{
+    Write($"{i+1}, ");
+    i = i + 2;
+}
+Write($"{i+1}");
