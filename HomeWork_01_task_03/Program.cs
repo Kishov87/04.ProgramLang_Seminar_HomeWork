a8d2328ee227b43e6caca8f4959d48d4e3364b2f
Pavel Kishov <kishov.p@gmail.com>
2022-06-15 23:37:13 +0300

    completed HomeWork_01_task_03

--- HomeWork_01_task_03/Program.cs
+++ HomeWork_01_task_03/Program.cs
@@ -2,3 +2,18 @@
 //4 -> да
 //-3 -> нет
 //7 -> нет
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int number1 = Convert.ToInt32(ReadLine());
+int A = 2;
+int N = number1%A;
+
+string answer = N == 0 
+?"Да. Число кратное - юююхууу!!!"
+:$"Нет. Число не кратное - бууу(((";
+
+WriteLine(answer);
