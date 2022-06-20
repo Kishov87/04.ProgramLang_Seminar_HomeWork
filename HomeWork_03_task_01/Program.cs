f50749d579895dee33987e3062d986e732be8ff5
Pavel Kishov <kishov.p@gmail.com>
2022-06-20 23:02:47 +0300

    completed HomeWork_03_task_01

---
+++ HomeWork_03_task_01/Program.cs
@@ -0,0 +1,28 @@
+﻿// // Задача 19
+// // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// // 14212 -> нет
+// // 12821 -> да
+// // 23432 -> да
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите пятизначое число: ");
+int Number = Convert.ToInt32(ReadLine());
+int j = 10000;
+int NewNumber = 0;
+int PreNumber = Number;
+
+if (Number < 10000 || Number > 99999)
+{
+    WriteLine($"{Number} - не пятизначое число!");
+    return;
+}
+while (PreNumber>0)
+{
+    NewNumber+=(PreNumber%10)*j;
+    PreNumber/=10;
+    j/=10;
+}
+WriteLine(NewNumber==Number ?$"{Number} -> палиндром" :$"{Number} -> не палиндром");
