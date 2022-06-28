478f0ffcc5b7e7ec6edb5f4e7c753be39938e9b2
Pavel Kishov <kishov.p@gmail.com>
2022-06-28 16:57:06 +0300

    completed HomeWork_05_task_01

--- HomeWork_05_task_01/Program.cs
+++ HomeWork_05_task_01/Program.cs
@@ -15,7 +15,7 @@ int[] GetRandomArray(int size, int minValue, int maxValue)
     int[] result=new int[size];
     for (int i = 0; i < size; i++)
     {
-        result[i] = new Random().Next(minValue, maxValue +1 );
+        result[i] = new Random().Next(minValue, maxValue +1);
     }
     return result;
 }
