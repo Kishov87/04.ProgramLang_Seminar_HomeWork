b2395db80cbb93e51495dcb986a8de15f0c984f7
Pavel Kishov <kishov.p@gmail.com>
2024-02-05 14:54:53 +0300

    Создание кода

---
+++ HomeWork_10_task_02/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 61: Показать треугольник Паскаля. *Сделать вывод в виде равнобедренного треугольника.
+
+using System;
+using static System.Console;
+class Program
+{
+    static void Main()
+    {
+        int numRows = 6; // Количество строк треугольника Паскаля
+
+        for (int i = 0; i < numRows; i++)
+        {
+            // Выводим пробелы для выравнивания треугольника
+           Write(new string(' ', numRows - i));
+
+            int number = 1;
+            for (int j = 0; j <= i; j++)
+            {
+                Write(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+
+            WriteLine();
+        }
+    }
+}
